bed4d3e8060689c6a140ef6d54a9cb1b1367f957
Crdzbird <luisalfonsocb83@gmail.com>
2023-10-21 22:15:13 +0200

    Minor changes

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,4 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root-app")
-)
+ReactDOM.render( <React.StrictMode> <HashRouter> <App /> </HashRouter> </React.StrictMode>, document.getElementById('root-app') );
